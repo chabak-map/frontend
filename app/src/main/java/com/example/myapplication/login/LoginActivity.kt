e1448daf26ac23ab81631b034d438f4a28048da1
kimjihw <kimal846@naver.com>
2021-11-18 17:52:58 +0900

    layout

--- app/src/main/java/com/example/myapplication/login/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/login/LoginActivity.kt
@@ -1,12 +1,20 @@
 package com.example.myapplication.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.R
+import com.example.myapplication.config.BaseActivity
+import com.example.myapplication.databinding.ActivityLoginBinding
+import com.example.myapplication.join.JoinActivity
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_login)
+		setContentView(binding.root)
+
+		binding.gotoJoinTv.setOnClickListener {
+			startActivity(Intent(this, JoinActivity::class.java))
+		}
 	}
 }

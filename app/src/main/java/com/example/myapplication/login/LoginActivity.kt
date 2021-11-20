790704c5db56bef0ee4852b2ac8a374670f277b9
kimjihw <kimal846@naver.com>
2021-11-20 15:53:45 +0900

    layout_modify

--- app/src/main/java/com/example/myapplication/login/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/login/LoginActivity.kt
@@ -1,20 +1,29 @@
 package com.example.myapplication.login
 
+import android.app.Dialog
+import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.example.myapplication.MainActivity
 import com.example.myapplication.R
 import com.example.myapplication.config.BaseActivity
 import com.example.myapplication.databinding.ActivityLoginBinding
 import com.example.myapplication.join.JoinActivity
+import java.util.*
 
-class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
+class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate){
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(binding.root)
 
+		
 		binding.btnLogin.setOnClickListener {
+
 			startActivity(Intent(this, MainActivity::class.java))
 		}
 	}

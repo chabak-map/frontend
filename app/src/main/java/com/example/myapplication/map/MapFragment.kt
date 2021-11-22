76ebbc4a873b558c6be175c7b6aefbe95329d107
kimjihw <kimal846@naver.com>
2021-11-23 01:57:52 +0900

    map

--- app/src/main/java/com/example/myapplication/map/MapFragment.kt
+++ app/src/main/java/com/example/myapplication/map/MapFragment.kt
@@ -1,36 +1,57 @@
 package com.example.myapplication.map
 
 import android.Manifest
+import android.location.Location
+import android.location.LocationListener
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.NonNull
 import androidx.core.app.ActivityCompat
 import com.example.myapplication.R
 import com.example.myapplication.config.BaseFragment
 import com.example.myapplication.databinding.FragmentMapBinding
-import com.naver.maps.map.LocationTrackingMode
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
-import com.naver.maps.map.UiSettings
+import com.naver.maps.map.*
 import com.naver.maps.map.util.FusedLocationSource
+import kotlinx.android.synthetic.main.activity_map.*
+import kotlinx.android.synthetic.main.fragment_map.*
+import kotlinx.android.synthetic.main.fragment_map.map
 import java.security.Permissions
 
 class MapFragment : BaseFragment<FragmentMapBinding>(FragmentMapBinding::bind, R.layout.fragment_map),
 OnMapReadyCallback{
 
-	private val PERMISSION_REQUEST_CODE = 1000
-	private lateinit var mlocationSource : FusedLocationSource
-	private lateinit var mnaverMap: NaverMap
+	private lateinit var locationSource : FusedLocationSource
+	private lateinit var naverMap: NaverMap
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
-		mlocationSource = FusedLocationSource(this, PERMISSION_REQUEST_CODE)
+
+		locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
 	}
 
-	override fun onMapReady(p0: NaverMap) {
-		mnaverMap.locationSource = mlocationSource
-		mnaverMap.locationTrackingMode = LocationTrackingMode.Follow
+	override fun onRequestPermissionsResult(
+		requestCode: Int,
+		permissions: Array<out String>,
+		grantResults: IntArray
+	) {
+		if(locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)){
+			if(!locationSource.isActivated){
+				naverMap.locationTrackingMode = LocationTrackingMode.None // 권한 거부
+			}
+			return
+		}
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+	}
+
+	override fun onMapReady(naverMap: NaverMap) {
+		this.naverMap = naverMap
+		naverMap.locationSource = locationSource
+	}
+
+	companion object {
+		private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
 	}
 }

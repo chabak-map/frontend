c8cbdc67fea420d3553563774518b6e71bc68719
kimjihw <kimal846@naver.com>
2021-11-25 20:04:14 +0900

    modify

--- app/src/main/java/com/example/myapplication/post/place/PlaceFragment.kt
+++ app/src/main/java/com/example/myapplication/post/place/PlaceFragment.kt
@@ -1,18 +1,21 @@
 package com.example.myapplication.post.place
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.example.myapplication.R
 import com.example.myapplication.config.ApplicationClass
 import com.example.myapplication.config.BaseFragment
+import com.example.myapplication.databinding.FragmentPlaceBinding
 import com.example.myapplication.post.place.models.PlaceRetrofitInterface
 import com.example.myapplication.post.place.models.place
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class PlaceFragment : BaseFragment<FragmentPostBinding>(FragmentPostBinding::bind, R.layout.fragment_place) {
-
+class PlaceFragment : BaseFragment<FragmentPlaceBinding>(FragmentPlaceBinding::bind, R.layout.fragment_place) {
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 
@@ -22,9 +25,12 @@ class PlaceFragment : BaseFragment<FragmentPostBinding>(FragmentPostBinding::bin
 	fun tryGetPlace(placeId : Int){
 		val placeRetrofitInterface = ApplicationClass.sRetrofit.create(PlaceRetrofitInterface::class.java)
 		placeRetrofitInterface.getPlace(placeId).enqueue(object : Callback<place>{
+			@SuppressLint("SetTextI18n")
 			override fun onResponse(call: Call<place>, response: Response<place>) {
 				var result = response.body() as place
 				binding.postPlaceNameTv.text = result.result.name
+				binding.postPlaceAddressTv.text = result.result.address
+				binding.postPlaceReviewCntTv.text = "${R.string.review_cnt} " + result.result.reviewCount.toString()
 			}
 
 			override fun onFailure(call: Call<place>, t: Throwable) {

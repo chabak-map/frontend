788dc534bde8c51d974dc0162076c95056cc102d
kimjihw <kimal846@naver.com>
2021-12-03 06:56:40 +0900

    postdetail

--- app/src/main/java/com/example/myapplication/map/detail/DetailPostActivity.kt
+++ app/src/main/java/com/example/myapplication/map/detail/DetailPostActivity.kt
@@ -3,19 +3,39 @@ package com.example.myapplication.map.detail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.R
+import com.example.myapplication.config.ApplicationClass
 import com.example.myapplication.config.BaseActivity
 import com.example.myapplication.databinding.ActivityDetailPostBinding
 import com.example.myapplication.map.adapter.RadiusPlaceRecyclerView
+import com.example.myapplication.map.detail.models.DetailPost
+import com.example.myapplication.map.detail.models.DetailPostRetrofitInterface
 import com.example.myapplication.map.models.RadiusPlace
 import com.example.myapplication.map.models.Result
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class DetailPostActivity : BaseActivity<ActivityDetailPostBinding>(ActivityDetailPostBinding::inflate) {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(binding.root)
 
-		var datas = intent.getSerializableExtra("data")
+		val datas = intent.getSerializableExtra("data")
 
-		println(datas)
+		tryGetDetailPost(datas as Int)
+	}
+
+	fun tryGetDetailPost(postId : Int){
+		val detailPostRetrofitInterface = ApplicationClass.sRetrofit.create(DetailPostRetrofitInterface::class.java)
+		detailPostRetrofitInterface.tryGetDetailPost(postId).enqueue(object : Callback<DetailPost>{
+			override fun onResponse(call: Call<DetailPost>, response: Response<DetailPost>) {
+				val result = response.body() as DetailPost
+				binding.detailPostTitleTv.text = result.result.title
+			}
+
+			override fun onFailure(call: Call<DetailPost>, t: Throwable) {
+				showCustomToast("${t.message}")
+			}
+		})
 	}
 }

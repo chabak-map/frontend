ce97732b1a362ebe373a7c59d59bab7442eb1c85
kimjihw <kimal846@naver.com>
2021-12-03 16:14:11 +0900

    Update PostPagerAdapter.kt

--- app/src/main/java/com/example/myapplication/map/detail/adapter/PostPagerAdapter.kt
+++ app/src/main/java/com/example/myapplication/map/detail/adapter/PostPagerAdapter.kt
@@ -2,34 +2,35 @@ package com.example.myapplication.map.detail.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.myapplication.R
 import com.example.myapplication.map.detail.models.DetailPost
+import com.example.myapplication.map.detail.models.Result
 import kotlinx.android.synthetic.main.detail_post_item.view.*
 
-class PostPagerAdapter(private val context: Context, postList : ArrayList<String>) :
+class PostPagerAdapter(private val context:Context, private val items : ArrayList<String>) :
 	RecyclerView.Adapter<PostPagerAdapter.PagerViewHolder>() {
-
-	var item = postList
 
 	override fun onCreateViewHolder(
 		parent: ViewGroup,
 		viewType: Int
-	): PostPagerAdapter.PagerViewHolder = PagerViewHolder(parent)
+	): PostPagerAdapter.PagerViewHolder = PagerViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.detail_post_item, parent, false))
+
 
 	override fun onBindViewHolder(holder: PostPagerAdapter.PagerViewHolder, position: Int) {
-		Glide.with(context).load(item[position]).into(holder.detailPost)
+		Glide.with(context).load(items[position]).into(holder.img)
 	}
 
 	override fun getItemCount(): Int {
-		return item.size
+		return items.size
 	}
 
-	inner class PagerViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
-		LayoutInflater.from(parent.context).inflate(R.layout.detail_post_item, parent, false)){
+	inner class PagerViewHolder(view : View) : RecyclerView.ViewHolder(view){
+		val img : ImageView = view.findViewById(R.id.detail_post)
 
-		val detailPost = itemView.detail_post
 	}
 }

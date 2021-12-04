ab73137c02900d32680dd3eb4a299033256a1d3b
JJJJisun <42526264+JJJJisun@users.noreply.github.com>
2021-12-05 01:03:10 +0900

    Post Layout

--- app/src/main/java/com/example/myapplication/find/FindActivity.kt
+++ app/src/main/java/com/example/myapplication/find/FindActivity.kt
@@ -1,12 +1,18 @@
 package com.example.myapplication.find
 
 import android.os.Bundle
-import com.example.myapplication.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.example.myapplication.config.BaseActivity
 import com.example.myapplication.databinding.ActivityFindBinding
-import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class FindActivity : BaseActivity<ActivityFindBinding>(ActivityFindBinding::inflate) {
+
+    val tabTextList = arrayListOf("아이디 찾기", "비밀번호 찾기")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -16,36 +22,26 @@ class FindActivity : BaseActivity<ActivityFindBinding>(ActivityFindBinding::infl
             finish()
         }
 
-        binding.tabLayoutFind.selectedTabPosition
+        binding.findFrame.adapter = ScreenSlidePagerAdapter(this)
+        binding.findFrame.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
-        binding.tabLayoutFind.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                when (tab?.position) {
+        TabLayoutMediator(binding.tabLayoutFind, binding.findFrame) { tab, position ->
+            tab.text = tabTextList[position]
+        }.attach()
 
-                    // 아이디 찾기 프레그먼트
-                    0 -> {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.find_frame, FindIdFragment())
-                            .commit()
-                    }
-                    // 비밀번호 찾기 프레그먼트
-                    1 -> {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.find_frame, FindPwFragment())
-                            .commit()
-                    }
-                }
+    }
+
+    private inner class ScreenSlidePagerAdapter(fm: FragmentActivity) : FragmentStateAdapter(fm) {
+        override fun getItemCount(): Int {
+            return tabTextList.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                0 -> FindIdFragment()
+                else -> FindPwFragment()
             }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-
-            }
-        })
-
+        }
     }
 
 }
--- app/src/main/java/com/example/myapplication/post/PostFragment.kt
+++ app/src/main/java/com/example/myapplication/post/PostFragment.kt
@@ -2,9 +2,7 @@ package com.example.myapplication.post
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
@@ -19,6 +17,7 @@ import com.google.android.material.tabs.TabLayoutMediator
 class PostFragment : BaseFragment<FragmentPostBinding>(FragmentPostBinding::bind, R.layout.fragment_post) {
 
 	val tabTextList = arrayListOf("포스트", "나의 포스트")
+
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 
